122173d2d5124de6c04cb62e5c2bb5144f32c635
David-Verde <david.verde.alvarez@gmail.com>
2025-09-15 15:19:44 -0500

    feat: implement transfer creation module

---
+++ app/controllers/transferencias_controller.rb
@@ -0,0 +1,32 @@
+class TransferenciasController < ApplicationController
+  before_action :set_articulo
+
+  def new
+    @transferencia = @articulo.transferencias.new
+    @personas = Persona.all
+  end
+
+  def create
+    @transferencia = @articulo.transferencias.new(transferencia_params)
+    
+    ActiveRecord::Base.transaction do
+      @articulo.update!(persona_id: @transferencia.persona_id)
+      @transferencia.save!
+    end
+    
+    redirect_to @articulo, notice: "Artículo transferido con éxito."
+  rescue ActiveRecord::RecordInvalid
+    @personas = Persona.all
+    render :new, status: :unprocessable_entity
+  end
+
+  private
+
+  def set_articulo
+    @articulo = Articulo.find(params[:articulo_id])
+  end
+
+  def transferencia_params
+    params.require(:transferencia).permit(:persona_id)
+  end
+end
