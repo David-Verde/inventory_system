01186b4f016453dc2fe39a68c4206e0ffff1df99
David-Verde <david.verde.alvarez@gmail.com>
2025-09-29 18:41:23 -0500

    fix test_helper for rubocop

--- test/support/authentication_helper.rb
+++ test/support/authentication_helper.rb
@@ -1,18 +1,19 @@
 module AuthenticationHelpers
+
   def sign_in_as(user)
-
     visit new_session_url
-
-
-    assert_selector "h1", text: "Sign in"
-
     fill_in "Correo electrónico", with: user.email_address
     fill_in "Password", with: "password"
-
-
     click_on "Sign in"
-
-
     assert_text "Inicio de sesión exitoso."
+  end
+
+  def login_as(user)
+    post session_path, params: {
+      user: {
+        email_address: user.email_address,
+        password: "password"
+      }
+    }
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,13 +6,11 @@ require "rails/test_help"
 Dir[Rails.root.join("test/support/**/*.rb")].sort.each { |f| require f }
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
+
   parallelize(workers: :number_of_processors)
 
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  fixtures :all
 end
 
 
